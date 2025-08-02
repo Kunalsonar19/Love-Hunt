bc5ca1f01b52dd39c4883d9c6f8d55094a0a4353
Kunalsonar19 <kunalwankhede958@gmail.com>
2025-08-02 20:46:20 +0530

    New Commit

--- src/game.ts
+++ src/game.ts
@@ -1,35 +1,36 @@
 import 'phaser';
-
 import { MainScene } from './scenes/MainScene';
 
-const config: GameConfig = {
+const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: window.innerWidth,
-	height: window.innerHeight,
-	input: { keyboard: true },
 	parent: 'game',
-	physics: {
-		arcade: {
-			debug: false,
-			gravity: { y: 1850 }
-		},
-		default: 'arcade'
-	},
-	scale: {
-		width: 1536,
-		height: 864,
-		autoCenter: Phaser.Scale.CENTER_BOTH,
-		autoRound: true,
-		mode: Phaser.Scale.FIT,
-		zoom: window.innerWidth / 1536
-	},
 	scene: [MainScene],
+	scale: {
+		mode: Phaser.Scale.RESIZE, // This makes the game resize to match the window
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: window.innerWidth,
+		height: window.innerHeight
+	},
 	render: {
 		antialias: false,
 		pixelArt: true,
 		roundPixels: true,
 		powerPreference: 'high-performance'
+	},
+	input: { keyboard: true },
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 1850 },
+			debug: false
+		}
 	}
 };
 
+// Initialize game
 const game = new Phaser.Game(config);
+
+// Optional: Listen for window resize and resize the game accordingly
+window.addEventListener('resize', () => {
+	game.scale.resize(window.innerWidth, window.innerHeight);
+});
